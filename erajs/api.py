6f07274e1d26ea007babf2951cf0402c16481714
何雨航 <miswanting@users.noreply.github.com>
2018-10-01 17:20:49 +0800

    Clean

--- erajs/api.py
+++ erajs/api.py
@@ -1,26 +1,10 @@
 # coding:utf-8
 from . import game as g
 
-# from . import engine as e
-# init = g.init
-# init_done = g.init_done
-# title = g.title
-# t = g.t
-# b = g.b
-# h = g.h
-# progress = g.progress
-# input = g.input
-# page = g.page
-# goto = g.goto
-# back = g.back
-# repeat = g.repeat
-# mode = g.mode
-# clear = g.clear
 data = {}
 
+
 # 显示控制
-
-
 def init():
     global data
     data = g.init()
