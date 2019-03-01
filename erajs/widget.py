db4124b74dfea1b47a3891d3dea5d3833f352e5f
Miswanting <miswanting@users.noreply.github.com>
2019-03-01 21:48:21 +0800

    Update widget.py

--- erajs/widget.py
+++ erajs/widget.py
@@ -1,6 +1,27 @@
+from . import engine as e
+
+
 class Widget:
-    pass
+    def __init__(self):
+        self.data = {
+            'hash': e.new_hash(),
+            'child': []
+        }
 
 
 class Button(Widget):
-    pass
+    def __init__(self, data):
+        super()
+        self.data['type'] = 'button'
+
+
+class Link(Widget):
+    def __init__(self, data):
+        super()
+        self.data['type'] = 'link'
+
+
+class Popup(Widget):
+    def __init__(self, data):
+        super()
+        self.data['type'] = 'popup'
