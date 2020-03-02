2231afdf6323f87cfda8eeb0b486abcd1e031ed0
(ง •_•)ง <miswanting@users.noreply.github.com>
2020-03-02 22:34:59 +0800

    先进化

--- erajs/Managers/DebugManager.py
+++ erajs/Managers/DebugManager.py
@@ -6,36 +6,36 @@ from ..Prototypes import Singleton
 
 
 class DebugManager(Singleton.Singleton):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         formatter = logging.Formatter('')
         stream_handler = logging.StreamHandler(sys.stdout)
         stream_handler.setFormatter(formatter)
-        file_handler = logging.FileHandler('Back.log', 'w', 'utf-8')
+        file_handler = logging.FileHandler('Erajs.log', 'w', 'utf-8')
         file_handler.setFormatter(formatter)
-        self.logger = logging.getLogger('logger')
-        self.logger.setLevel(logging.DEBUG)
-        if len(self.logger.handlers) == 0:
-            self.logger.addHandler(stream_handler)
-            self.logger.addHandler(file_handler)
+        self.__logger = logging.getLogger('logger')
+        self.__logger.setLevel(logging.DEBUG)
+        if len(self.__logger.handlers) == 0:
+            self.__logger.addHandler(stream_handler)
+            self.__logger.addHandler(file_handler)
 
-    def debug(self, text):
+    def debug(self, text: str = '') -> None:
         self.print('DEBG', text)
 
-    def info(self, text):
+    def info(self, text: str = '') -> None:
         self.print('INFO', text)
 
-    def warn(self, text):
+    def warn(self, text: str = '') -> None:
         self.print('WARN', text)
 
-    def error(self, text):
+    def error(self, text: str = '') -> None:
         self.print('ERRO', text)
 
-    def critical(self, text):
+    def critical(self, text: str = '') -> None:
         self.print('!!!!', text)
 
-    def print(self, logo, text):
+    def print(self, logo: str = 'DEBG', text: str = '') -> None:
         temp = '[{}]({}){}'
         date = datetime.today()
         text = temp.format(logo, date.strftime("%y%m%d-%H%M%S-%f"), text)
-        self.logger.debug(text)
+        self.__logger.debug(text)
