f0b44c494ea82e33ceca66c32bbacbae7023f637
(ง •_•)ง <miswanting@users.noreply.github.com>
2020-03-02 22:35:17 +0800

    升版

--- erajs/Managers/LockManager.py
+++ erajs/Managers/LockManager.py
@@ -10,36 +10,36 @@ class LockManager:
     # -1：_unlock_forever()：鼠标右键；
     __lock_status = [0, 'mouse']
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
 
-    def wait_for_unlock(self):
+    def wait_for_unlock(self) -> None:
         # print('wait_for_unlock')
         while self.is_locked():
             time.sleep(0.1)
 
-    def is_locked(self):
+    def is_locked(self) -> None:
         # print('is_locked')
         if self.__lock_status[0] == 1:
             return True
         else:
             return False
 
-    def lock_passed(self):
+    def lock_passed(self) -> None:
         # print('lock_passed')
         if self.__lock_status[0] == -1:
             return True
         else:
             return False
 
-    def lock(self):
+    def lock(self) -> None:
         # print('lock')
         self.__lock_status[0] = 1
 
-    def unlock(self):
+    def unlock(self) -> None:
         # print('unlock')
         self.__lock_status[0] = 0
 
-    def unlock_forever(self):
+    def unlock_forever(self) -> None:
         # print('unlock_forever')
         self.__lock_status[0] = -1
